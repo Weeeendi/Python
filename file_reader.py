e7fbcbfef3f2c2fb17074050d280fceac8ee662b
Weeeendi <wendi1078771091@gmail.com>
2019-02-06 09:25:11 +0800

    OPEN_FILES

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,7 @@
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
     print(contents)
+
+with open("text_files\pi_digits.txt") as file_object:
+    contents = file_object.read()
+    print(contents)
