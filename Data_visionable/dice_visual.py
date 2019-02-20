34d1cf4cf4399262a77f8c6af5d6e8e4165fcdac
Weeeendi <wendi1078771091@gmail.com>
2019-02-20 21:58:55 +0800

    Apirl20

---
+++ Data_visionable/dice_visual.py
@@ -0,0 +1,31 @@
+import pygal
+from die import Die
+
+#创建两个骰子
+die_1 = Die()
+die_2 = Die()
+
+#掷几次色子,并将结果储存在一个列表中
+results=[]
+for roll_num in range(1000):
+    result = die_1.roll()+die_2.roll()
+    results.append(result)
+
+# 分析结果
+frequencies = []
+max_results=die_1.num_sides+die_2.num_sides
+for value in range(2, max_results + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_lables = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6+D6', frequencies)
+hist.render_to_file('dice_visual.svg')
--- Data_visionable/random_walk.py
+++ Data_visionable/random_walk.py
@@ -9,18 +9,22 @@ class RandomWalk():
         self.x_values = [0]
         self.y_values = [0]
     
+    def get_step(self):
+        """确定方向和步长"""
+         direction = choice([1, -1])
+         distance = choice([0, 1, 2, 3, 4])
+         step = direction * distance
+         return step
+         
+
     def fill_walk(self):
         """计算随机漫步所包含的所有点"""
 
         #不断漫步，直到列表达到指定的长度
         while len(self.x_values) < self.num_points:
-             x_direction = choice([1, -1])
-             x_distance = choice([0, 1, 2, 3, 4])
-             x_step = x_direction * x_distance
-            
-             y_direction = choice([1, -1])
-             y_distance = choice([0, 1, 2, 3, 4])
-             y_step = y_direction * y_distance
+          
+             x_step = self.get_step()
+             y_step = self.get_step()
 
              #拒绝原地踏步
              if x_step == 0 and y_step == 0:
